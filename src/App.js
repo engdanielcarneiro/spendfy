ec76ba165bd8740f0be32919fb681ea7e1bddfa4
Daniel Carneiro <daniel.alexandre97@hotmail.com>
2024-06-10 22:00:11 -0300

    feat: first page. Login components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="page">
+      <header>
+        <h1 className="title">Save time & money with Spendfy</h1>
       </header>
+      <div className="formContainer">
+        <form className="form" autoComplete="off">
+          <input className="formControl" placeholder="Name"></input>
+          <input className="formControl" placeholder="Last name"></input>
+          <input className="formControl" placeholder="E-mail address"></input>
+          <input className="formControl" placeholder="Password"></input>
+          <button className="formButton">Login</button>
+        </form>
+        <span className="labelContainer">
+          <label className="label">
+            By clicking the button, you are agreeing to our Terms and Services
+          </label>
+        </span>
+      </div>
     </div>
   );
 }
